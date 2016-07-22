74710dad6f39ba3a8a29fa8bfc47653647a0c97e
Adam Luchini <aluchini@gmail.com>
2016-07-22 14:09:41 -0700

    band form and add band

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -16,6 +16,19 @@ namespace BandTracker
         List<Band> allBands = Band.GetAll();
         return View["bands.cshtml", allBands];
       };
+      Get["/venues"] = _ => {
+        List<Venue> allVenues = Venue.GetAll();
+        return View["venues.cshtml", allVenues];
+      };
+      Get["/bands/add"] = _ => {
+        return View["band_add.cshtml"];
+      };
+      Post["/bands/add"] = _ => {
+        Band newBand = new Band(Request.Form["band_name"]);
+        newBand.Save();
+        List<Band> allBands = Band.GetAll();
+        return View["bands.cshtml", allBands];
+      };
     }
   }
 }
